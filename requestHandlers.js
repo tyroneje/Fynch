a071071c210643b13677bb294a6dcf1bafe49d80
tyroneje <tyroneedwards08@yahoo.com>
2024-05-29 13:16:46 -0400

    returms stock and calculations

--- requestHandlers.js
+++ requestHandlers.js
@@ -10,8 +10,18 @@ let stock_data_fx = 'function=TIME_SERIES_MONTHLY_ADJUSTED';
 let bob = {};
 let current_year_monthly_dividends = [];
 
+let mock_IBM_data = {
+    "Symbol": "IBM",
+    "AssetType": "Common Stock",
+    "Name": "International Business Machines",
+    "DividendPerShare": "6.64",
+    "DividendYield": "0.0391",
+    "DividendDate": "2024-06-10",
+    "ExDividendDate": "2024-05-09"
+}
+
 module.exports = {
-    stockAmountRequestHandler: function (req, res) {
+    stockAmountRequestHandler: async function (req, res) {
         if (!req.query.stock || !req.query.goal) {
             console.log('error....');
             res.send({
@@ -23,15 +33,20 @@ module.exports = {
             let goal = req.query.goal;
             let getCompanyInfoURL = url + `?${key}&${company_fx}&${symbol(stock)}`;
 
-            fetch(getCompanyInfoURL).then(async (res) => {
-                let returns = res.body.pipeThrough(new TextDecoderStream()).getReader();
-                let arr = {};
+            let calculateStockAmount = (stock, amount) => {
+                let result = amount / stock.dividend;
+                return result;
+            }
 
-                while (true) {
+            await fetch(getCompanyInfoURL).then(async (res) => {
+                let returns = res.body.pipeThrough(new TextDecoderStream()).getReader();
+                let arr = mock_IBM_data;
+
+                /*while (true) {
                     const { value, done } = await returns.read();
                     if (done) break;
                     arr = JSON.parse(value);
-                }
+                }*/
 
                 bob = {
                     symbol: arr.Symbol,
@@ -40,77 +55,79 @@ module.exports = {
                     date: arr.DividendDate,
                     exDate: arr.ExDividendDate
                 };
+
             });
+            console.log(bob);
+            res.status(200).send({ ...bob, amountToBuy: calculateStockAmount(bob, goal).toFixed(2) });
 
-            let getAdjustedMonthlyReturnsURL = url + `?${key}&${stock_data_fx}&${symbol(stock)}`;
+           // let getAdjustedMonthlyReturnsURL = url + `?${key}&${stock_data_fx}&${symbol(stock)}`;
 
-            fetch(getAdjustedMonthlyReturnsURL).then(async (res1) => {
-                let returns = res1.body.pipeThrough(new TextDecoderStream()).getReader();
-                let company_adjusted_returns;
+            // fetch(getAdjustedMonthlyReturnsURL).then(async (res1) => {
+            //     let returns = res1.body.pipeThrough(new TextDecoderStream()).getReader();
+            //     let company_adjusted_returns;
 
-                while (true) {
-                    const { value, done } = await returns.read();
-                    if (done) break;
-                    company_adjusted_returns = JSON.parse(value);
-                }
+            //     while (true) {
+            //         const { value, done } = await returns.read();
+            //         if (done) break;
+            //         company_adjusted_returns = JSON.parse(value);
+            //     }
 
-                //get dates and related data for the current year
-                for (const key in company_adjusted_returns['Monthly Adjusted Time Series']) {
-                    let date = new Date(key);
+            //     //get dates and related data for the current year
+            //     for (const key in company_adjusted_returns['Monthly Adjusted Time Series']) {
+            //         let date = new Date(key);
 
-                    if (date > new Date(`01/01/${new Date().getFullYear()}`)) {
-                        if (company_adjusted_returns['Monthly Adjusted Time Series'][key]['7. dividend amount'] > 0.00) {
-                            current_year_monthly_dividends.push({ date: key, ...company_adjusted_returns['Monthly Adjusted Time Series'][key] });
-                        }
-                    }
-                    else {
-                        break;
-                    }
-                }
+            //         if (date > new Date(`01/01/${new Date().getFullYear()}`)) {
+            //             if (company_adjusted_returns['Monthly Adjusted Time Series'][key]['7. dividend amount'] > 0.00) {
+            //                 current_year_monthly_dividends.push({ date: key, ...company_adjusted_returns['Monthly Adjusted Time Series'][key] });
+            //             }
+            //         }
+            //         else {
+            //             break;
+            //         }
+            //     }
 
-                //calculate payout term
-                let dividend_lapse = (dividend_list) => {
-                    let div_span = 0;
-                    let b = 1;
-                    for (let index = 0; index < dividend_list.length - 2; index++) {
-                        let prev_div = new Date(dividend_list[index]['date']).getMonth() + 1;
-                        let post_div = new Date(dividend_list[b]['date']).getMonth() + 1;
-                        let span = prev_div - post_div;
+            //     //calculate payout term
+            //     let dividend_lapse = (dividend_list) => {
+            //         let div_span = 0;
+            //         let b = 1;
+            //         for (let index = 0; index < dividend_list.length - 2; index++) {
+            //             let prev_div = new Date(dividend_list[index]['date']).getMonth() + 1;
+            //             let post_div = new Date(dividend_list[b]['date']).getMonth() + 1;
+            //             let span = prev_div - post_div;
 
-                        if (span == 0) continue;
-                        else {
-                            if (span > div_span) {
-                                div_span = span;
-                            }
-                        }
-                    }
-                    return div_span;
-                }
+            //             if (span == 0) continue;
+            //             else {
+            //                 if (span > div_span) {
+            //                     div_span = span;
+            //                 }
+            //             }
+            //         }
+            //         return div_span;
+            //     }
 
-                let getPayoutTerm = (lapse_amount) => {
-                    switch (lapse_amount) {
-                        case 1:
-                            return 12;
-                        case 3:
-                            return 4;
-                        case 6:
-                            return 2;
-                        default:
-                            break;
-                    }
-                }
+            //     let getPayoutTerm = (lapse_amount) => {
+            //         switch (lapse_amount) {
+            //             case 1:
+            //                 return 12;
+            //             case 3:
+            //                 return 4;
+            //             case 6:
+            //                 return 2;
+            //             default:
+            //                 break;
+            //         }
+            //     }
 
-                let payout = getPayoutTerm(dividend_lapse(current_year_monthly_dividends));
+            //     let payout = getPayoutTerm(dividend_lapse(current_year_monthly_dividends));
 
 
-                let calculateStockAmount = (stock, amount) => {
-                    let result = amount / stock.dividend;
-                    return result;
-                }
-                //calculateStockAmount(bob, goal);
+            //     let calculateStockAmount = (stock, amount) => {
+            //         let result = amount / stock.dividend;
+            //         return result;
+            //     }
+            //     //calculateStockAmount(bob, goal);
 
-                res.send({ ...bob, amountToBuy: calculateStockAmount(bob, goal).toFixed(2) });
-            });
+            // });
 
 
             //pull or format stock given in query
